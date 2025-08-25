3bf01d91916224899107ab4da03a59036c035939
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-25 17:10:45 -0500

    Add the application skeleton

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <main className="min-h-dvh grid place-items-center">
+      <h1 className="text-3xl font-bold underline">ParkApp</h1>
+    </main>
+  )
+}
