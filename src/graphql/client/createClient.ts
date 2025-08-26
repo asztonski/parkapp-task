6113c22cd3d74f88affc1c3d8d5a50951bc27d7a
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-26 14:34:59 -0500

    Create the URQL client factory

---
+++ src/graphql/client/createClient.ts
@@ -0,0 +1,11 @@
+﻿import { createClient, cacheExchange, fetchExchange } from "urql";
+
+const url = import.meta.env.VITE_GRAPHQL_URL;
+
+export function makeUrqlClient() {
+  return createClient({
+    url,
+    exchanges: [cacheExchange, fetchExchange],
+    requestPolicy: "cache-first",
+  });
+}
