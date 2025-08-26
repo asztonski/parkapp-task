6e5b8135b3eaa181098870b1efc0fcb4c4eac682
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-25 19:47:53 -0500

    Add the ESLint flat config for React + TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,29 @@
+// @ts-check
 import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
+import hooks from 'eslint-plugin-react-hooks'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+      }
     },
-  },
-])
+    plugins: { react, 'react-hooks': hooks },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
+    settings: { react: { version: 'detect' } }
+  }
+]
