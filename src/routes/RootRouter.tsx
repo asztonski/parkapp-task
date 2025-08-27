e2ae04013a5abdb6d0d65c32679a78de8ff07b4e
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-27 14:14:43 -0500

    Add the RootRouter for Login, Pilot, and NotFound

---
+++ src/routes/RootRouter.tsx
@@ -0,0 +1,17 @@
+﻿import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from '../pages/Login'
+import Pilot from '../pages/PilotPage'
+import NotFound from '../pages/NotFoundPage'
+
+export default function RootRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/pilot" element={<Pilot />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
