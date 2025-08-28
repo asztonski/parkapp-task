2037eadca825b09af5e6195e1d45daef67c9e594
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-28 01:51:16 -0500

    Prepare the GH Pages setup and protect the pilot route

---
+++ src/types/ambient-fixes.d.ts
@@ -0,0 +1,14 @@
+﻿declare namespace global {
+  namespace WebAssembly {
+    interface Imports { [module: string]: any }
+    interface Instance { readonly exports: any }
+  }
+  interface SharedWorker {
+    readonly port: MessagePort;
+  }
+  var SharedWorker: {
+    prototype: SharedWorker;
+    new(scriptURL: string | URL, options?: { name?: string }): SharedWorker;
+  };
+}
+export {};
