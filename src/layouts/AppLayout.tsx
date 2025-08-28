8e3c24a4bac9873d03b072872a283526ada1d7f7
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-27 19:58:02 -0500

    Add the padding to the AppLayout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
-    <main className="w-full">
+    <main className="w-full px-10 py-8">
       <Outlet />
     </main>
   );
