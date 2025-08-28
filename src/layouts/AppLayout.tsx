a246e746db786d6a62334ec2d447ddf86a06c5b1
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-08-27 23:50:24 -0500

    Increase the vertical padding on the Pilot screen

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
-    <main className="w-full flex-1 px-10 py-8">
+    <main className="w-full flex-1 px-10 py-10">
       <Outlet />
     </main>
   );
